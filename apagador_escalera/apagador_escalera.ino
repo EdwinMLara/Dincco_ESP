184a0da6d7b21bf2bc2c47b2f730452a79f44747
EdwinMLara <emlara35@gmail.com>
2020-05-04 21:47:35 -0500

    creando codigo para apagador de escalera, y no se que putos le mobi al ESP server led, falta agregar codigo para envio de datos pero el algoritmo es funcional

--- apagador_escalera/apagador_escalera.ino
+++ apagador_escalera/apagador_escalera.ino
@@ -2,9 +2,6 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-//const char* ssid = "Edwin-Wifi";
-//const char* password = "Valiant.Shadow35";
-
 const char* ssid = "INSOEL-IoT 2";
 const char* password = "86723558";
 
@@ -15,25 +12,22 @@ IPAddress subnet(255, 255, 255, 0);
 WiFiServer server(80);
 uint8_t led_wifi = 2;
 
-uint8_t s1 = 5;
-bool Vs1 = 0;
-uint8_t s2 = 18;
-uint8_t s3 = 19;
-
 uint8_t led_moc1 = 23;
 byte moc1 = 22;
 
+uint8_t s1 = 5;
+bool status_s1 = 0;
+
+bool bandera, start;
 
 String header,estado;
 
 boolean sensor_apagador,aux_sensor_apagador;
-boolean bandera,start;
 boolean control_calendario_celda;
 
 unsigned long startmillis;
 unsigned long currentmillis;
 unsigned long tiempo_peticion = 1000;
-
 
 void connectar_to_ssid(){
   Serial.print("Conectado a : ");
@@ -62,6 +56,7 @@ void connectar_to_ssid(){
   Serial.println(WiFi.localIP());
 }
 
+
 void response(WiFiClient client,uint8_t s){
   
   if(client){
@@ -74,24 +69,15 @@ void response(WiFiClient client,uint8_t s){
               if(header.indexOf("/on") >= 0){
                 Serial.println("on");
                 estado = "on";
-                Vs1 = 1;
+                status_s1 = 1;
                 digitalWrite(s,HIGH);
               }else if(header.indexOf("/off") >=0){
                 Serial.println("off");
                 estado = "off";
-                Vs1 = 0;
+                status_s1 = 0;
                 digitalWrite(s,LOW);
-              }else if(header.indexOf("/control_on") >= 0){
-                Serial.println("Control por Calendario y Celda Activado");
-                control_calendario_celda = 1;
-                digitalWrite(s3,HIGH);
-                startmillis = millis();  
-              }else if(header.indexOf("/control_off") >= 0){
-                Serial.println("Control por Calendario y Celda Desactivado");
-                control_calendario_celda = 0;
-                digitalWrite(s3,LOW);  
               }
-
+              
               client.println("HTTP/1.1 200 OK"); 
               client.println("Access-Control-Allow-Origin:*");
               client.println("Access-Control-Allow-Methods:POST,GET");
@@ -99,7 +85,7 @@ void response(WiFiClient client,uint8_t s){
               client.println();
               
               client.println("<!DOCTYPE html><html>");
-              client.println("<body><h1>Lampara 1</h1>");
+              client.println("<body><h1>Control</h1>");
            
               if(estado=="off"){
                 client.println("<p>off</p>");
@@ -157,131 +143,49 @@ bool  ZeroCross(byte moc){
   return(Cross);
 }
 
-void status_variables(const char *host,const int httpPort,String url,uint8_t s[3]){
-  String json_response = request(host,httpPort,url);
-
-  StaticJsonDocument<400> doc;
-
-  DeserializationError error = deserializeJson(doc,json_response);
-  if(error){
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-  }
-
-  for(int i=0;i<doc.size();i++){
-    JsonObject object = doc[i].as<JsonObject>();
-    const char* aux = object["id_lampara"];
-    int status_lampara = (int)object["status"];
-    Serial.println(aux);
-
-    if(status_lampara){
-      digitalWrite(s[i],HIGH);
-    }else{
-      digitalWrite(s[i],LOW);
-    }
-  }
-  
-}
-
-bool control_general(bool sensor ,bool aux_sensor,uint8_t led_control,uint8_t s){
-  WiFiClient client;
-  const char* host = "192.168.0.16";   const int httpPort = 80; 
-  bool aux_sensor_apagador_int = aux_sensor;
-  
-  if (sensor == HIGH) {
-    if(bandera || start){
-      if(start){
-        aux_sensor_apagador_int = sensor;  
-      }
-      start = false;
-      bandera = false;
-      digitalWrite(led_control,HIGH);
-      String url = "Dinnco/update_status_lampara.php?control_manual=1&id_lampara=8";
-      request(host,httpPort,url);
+void actualizacionStatus(const char* host,const int httpPort,String url,uint8_t led,bool moc){
+  if(bandera || start){
+    start = false;
+    bandera = false;
+    digitalWrite(led,moc);
+    request(host,httpPort,url);
+    if(moc)
       Serial.println("Encendido");
-      delay(500);
-    }   
-  }else{  
-    if(bandera || start){
-      if(start){
-        aux_sensor_apagador_int = sensor_apagador;    
-      }
-      start = false;
-      bandera = false;
-      String url = "Dinnco/update_status_lampara.php?control_manual=0&id_lampara=8";
-      request(host,httpPort,url);
-      digitalWrite(led_control,LOW);
+    else
       Serial.println("Apagado");
-      delay(500); 
-    }else{
-      client = server.available();
-      response(client,s);
-      if(control_calendario_celda){
-         currentmillis = millis();
-         if(tiempo_peticion <= abs(currentmillis - startmillis)){
-            Serial.println("Revision de Encendido");
-            String url = "Dinnco/current_status_light.php?id_area=4";
-            String json_response = request(host,httpPort,url);
-
-            StaticJsonDocument<400> doc;
-
-            DeserializationError error = deserializeJson(doc,json_response);
-            if(error){
-              Serial.print(F("deserializeJson() failed: "));
-              Serial.println(error.c_str());
-            }
-
-            Serial.println(doc.size());
-            JsonArray json_array = doc.as<JsonArray>();
-            JsonObject object = doc[0].as<JsonObject>();
-            const char* aux = object["id_lampara"];
-            int status_lampara = (int)object["status"];
-            Serial.println(aux);
-
-            if(status_lampara){
-              Vs1=1;
-              }else{
-              Vs1=0;
-              }
-            digitalWrite(s,Vs1);
-            
-            
-            startmillis = millis();
-         }
-      }
-    }
   }
-
-  if(sensor !=  aux_sensor_apagador_int){
-    bandera = true;  
-  }
-
-  aux_sensor_apagador_int = sensor;
-  return aux_sensor_apagador_int;  
 }
 
-bool sensor_general(bool sensor,bool s){
-  bool aux = false;
+bool controlApagador(bool moc,bool aux_moc,uint8_t led){
+  WiFiClient client;
+  const char* host = "192.168.0.16";   
+  const int httpPort = 80;
+  String url = "";
   
-  if(sensor == 1 && s == 0){
-    aux = true;  
-  }
-  return aux;
+ if(moc != aux_moc){
+  Serial.println("Envio de datos"); 
+  aux_moc = moc;  
+ }
+ 
+ client = server.available();
+ response(client,led);
+
+ return aux_moc; 
+ 
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(led_wifi,OUTPUT);
   pinMode(s1,OUTPUT);
-  pinMode(s2,OUTPUT);
-  pinMode(s3,OUTPUT);
+
   pinMode(moc1,INPUT_PULLUP);
   pinMode(led_moc1,OUTPUT);
 
   bandera = false;  start = true;
-  
+
   tiempo_peticion *= 10;
-  
+
   connectar_to_ssid();
 
   server.begin();
@@ -289,13 +193,19 @@ void setup() {
   Serial.println();
   Serial.println("----------------------------------------------------");
   digitalWrite(led_wifi,HIGH);
+
 }
 
-void loop() { 
-  
+void loop() {
   sensor_apagador = ZeroCross(moc1);
-  Serial.println(sensor_apagador);
-  sensor_apagador = sensor_general(sensor_apagador,Vs1);
-  aux_sensor_apagador = control_general(sensor_apagador,aux_sensor_apagador,led_moc1,s1);
-  delay(500);
+  if(start){
+    aux_sensor_apagador = sensor_apagador;
+    start = false;
+  }
+  Serial.print(sensor_apagador);
+  Serial.print(" , ");
+  Serial.println(aux_sensor_apagador);
+
+  aux_sensor_apagador = controlApagador(sensor_apagador,aux_sensor_apagador,s1);
+  delay(3000);
 }
